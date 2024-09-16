7c5ba5081b9f48a299792c3f4fb67d52f1fa37ae
Harold Montoya <103783601+Ulfrcrazy@users.noreply.github.com>
2024-09-16 16:42:36 +0200

    my first game whit JavaScript

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let randomNum1 = Math.floor(Math.random() * 6 ) + 1;
+
+let randomImg = "dice" + randomNum1 + ".png";
+
+let randomImgSource = "images/" + randomImg;
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src",randomImgSource);
+
+let randomNum2 = Math.floor(Math.random() * 6) + 1;
+let randomImgSource2 = "images/dice" + randomNum2 + ".png";
+var img2 = document.querySelectorAll("img")[1].setAttribute("src",randomImgSource2);
+
+if(randomNum1 > randomNum2){
+ document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNum2 > randomNum1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
