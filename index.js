63c53f9a05c70a607499384ec52e2fc76cfdeba4
Harold Montoya <103783601+Ulfrcrazy@users.noreply.github.com>
2024-09-17 15:13:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let randomNum1 = Math.floor(Math.random() * 6 ) + 1;
 
 let randomImg = "dice" + randomNum1 + ".png";
 
-let randomImgSource = "assets/images/" + randomImg;
+let randomImgSource = "./assets/images/" + randomImg;
 
 var img1 = document.querySelectorAll("img")[0];
 
 img1.setAttribute("src",randomImgSource);
 
 let randomNum2 = Math.floor(Math.random() * 6) + 1;
-let randomImgSource2 = "assets/images/dice" + randomNum2 + ".png";
+let randomImgSource2 = "./assets/images/dice" + randomNum2 + ".png";
 var img2 = document.querySelectorAll("img")[1].setAttribute("src",randomImgSource2);
 
 if(randomNum1 > randomNum2){
